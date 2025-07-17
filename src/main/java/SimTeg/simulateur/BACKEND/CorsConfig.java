f1c8c2cd84282f6a875fc86fb11980d2023ee4f0
LUDIQELVI <brendaelvira183@gmail.com>
2025-07-17 19:54:07 +0100

    Modification des properties

--- src/main/java/SimTeg/simulateur/BACKEND/CorsConfig.java
+++ src/main/java/SimTeg/simulateur/BACKEND/CorsConfig.java
@@ -5,20 +5,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://frontend-vc3s.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+
+        config.setAllowedOrigins(Arrays.asList(
+                "https://frontend-vc3s.vercel.app",
+                "http://localhost:4200"
+        ));
+
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); 
+
+        return new CorsFilter(source);
     }
 }
